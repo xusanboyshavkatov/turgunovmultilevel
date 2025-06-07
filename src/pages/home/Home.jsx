f45ca701416628076ab04304eafd49697b70e36e
xusanboy_shavkatov <108666195+xusanboyshavkatov@users.noreply.github.com>
2025-06-07 15:05:00 +0500

    init date backend test
    init date backend test

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,13 @@
-import { React, useState, useEffect } from 'react'
+import { React, useState, useEffect, use } from 'react'
 import './home.css'
-import Footer from '../../components/footer/Footer'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import testimg from '../../assets/test.png'
+import Card from '../../components/card/Card';
+import axios from 'axios';
 
-const Home = () => {
+
+const Home = ({ footerHeight }) => {
 
   const [user, setUser] = useState(null);
 
@@ -11,34 +16,96 @@ const Home = () => {
 
     if (tg?.initDataUnsafe?.user) {
       setUser(tg.initDataUnsafe.user);
+      sendProfileData()
     } else {
       console.log("Telegram WebApp foydalanuvchi ma'lumotlari mavjud emas.");
     }
   }, []);
 
+  coconst sendProfileData = async () => {
+  try {
+    const response = await axios.post(
+      'https://mock.codearch.uz/api/user/profile',
+      {
+        first_name: `${user.first_name}`,
+        last_name: `${user.first_name}`,
+        photo:`${user.photo_url}`,
+        telegram_id: `${user.id}`
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      }
+    );
+
+    console.log('Success:', response.data);
+  } catch (error) {
+    console.error('Error:', error.response ? error.response.data : error.message);
+  }
+};
+
   return (
-    <div className='home'>
+    <div className='home' style={{ paddingBottom: footerHeight }}>
 
       <div className="home-navbar">
         {user?.photo_url && <img src={user.photo_url} alt="User" />}
         <div className="home-navbar-detalies">
           <h2>Good day👋</h2>
-          <h1>{user?.first_name} salom</h1>
+          <h1>{user?.first_name}</h1>
         </div>
       </div>
 
-      {user ? (
-        <div>
-          <h2 className="text-xl font-bold">Salom, {user.first_name}!</h2>
-          <p>ID: {user.id}</p>
-          <p>Username: @{user.username}</p>
-          <p>Til: {user.language_code}</p>
-          <img src={user.photo_url} alt="" />
+      <div className="home-search">
+        <div className="home-search-bar">
+          <FontAwesomeIcon icon={faMagnifyingGlass} className='home-search-icon' />
+          <input type="text" placeholder='Search here' />
         </div>
-      ) : (
-        <p>Ma'lumotlar yuklanmoqda yoki foydalanuvchi ma'lum emas...</p>
-      )}
-      <Footer></Footer>
+      </div>
+
+      <div className="home-categories">
+        <h1>Categories</h1>
+        <div className="home-categories-card">
+          <div
+            className="home-categories-card-item"
+            style={{
+              backgroundImage: `url(${testimg})`,
+              backgroundSize: 'cover',          // Rasmani divni to‘liq qamrab olishi uchun
+              backgroundPosition: 'center',     // Markazdan joylashishi uchun (ixtiyoriy)
+              backgroundRepeat: 'no-repeat'     // Takrorlanmasligi uchun (ixtiyoriy)
+            }}
+          >
+            <h2>Speaking</h2>
+
+          </div>
+          <div
+            className="home-categories-card-item"
+            style={{
+              backgroundImage: `url(${testimg})`,
+              backgroundSize: 'cover',          // Rasmani divni to‘liq qamrab olishi uchun
+              backgroundPosition: 'center',     // Markazdan joylashishi uchun (ixtiyoriy)
+              backgroundRepeat: 'no-repeat'     // Takrorlanmasligi uchun (ixtiyoriy)
+            }}
+          >
+            <h2>Writing</h2>
+
+          </div>
+        </div>
+      </div>
+
+      <div className="home-cards">
+        <div className="home-cards-reco-seeall">
+          <h1>Recommended</h1>
+          <a>See all</a>
+        </div>
+        <div className="home-cards-items">
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+        </div>
+      </div>
+
     </div>
   )
 }
